ba8198d6f81ab86e808be952990d0e5533364589
Maros Kollar <maros@k-1.com>
2016-12-27 21:46:33 +0100

    Update copyright

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*** WeatherUnderground Z-Way HA module *******************************************
 
 Version: 1.11
-(c) Maroš Kollár, 2015
+(c) Maroš Kollár, 2015-2017
 -----------------------------------------------------------------------------
 Author: Maroš Kollár <maros@k-1.com>
 Description:
